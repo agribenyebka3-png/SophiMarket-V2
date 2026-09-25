25644be17d9294560d73ad1bcb6c5c76655e8974
agribenyebka3-png <agribenyebka3@gmail.com>
2026-09-25 15:35:37 +0200

    Ajouter classe principale SophiMarket

---
+++ src/main/java/fr/sophi/market/SophiMarket.java
@@ -0,0 +1,12 @@
+package fr.sophi.market;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(SophiMarket.MOD_ID)
+public final class SophiMarket {
+    public static final String MOD_ID = "sophimarket";
+
+    public SophiMarket() {
+        System.out.println("[SophiMarket V2] Chargement du Market et des métiers.");
+    }
+}
